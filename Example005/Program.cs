fb837c3d9b5e22222266e4cad0480988e029ba5f
Artemii Belousov <artemii.belousov@mail.ru>
2023-03-23 12:50:34 +0500

    added folder Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
