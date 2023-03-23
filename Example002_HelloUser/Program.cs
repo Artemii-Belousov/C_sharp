7ac4268c943328b56f8eabc52aa9c81bf2048a9f
Artemii Belousov <artemii.belousov@mail.ru>
2023-03-23 10:25:38 +0500

    added folder Example002

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// Чтобы вывести данные в терминал с переводом на новую строку, необходимо ввести команду Console.WriteLine()
+// Чтобы вывести данные в терминал без перевода на новую строку, необходимо ввести команду Console.Write()
+Console.Write("Enter your name");
+// Чтобы считать данные, необходимо ввести команду Console.ReadLine()
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
