a4d89f0a075835c77c97fd41e688b9a9a5e1def5
Artemii Belousov <artemii.belousov@mail.ru>
2023-03-23 12:24:42 +0500

    added rendom in Example003 file

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,5 +1,7 @@
 ﻿// для задания целочисленной переменной использовать префикс int
-int numberA = 3;
-int numberB = 6;
+int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
 int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(result);
